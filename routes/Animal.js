2e2ade99266d0d4d93a269a3b20fd1e2e501a835
Carlos Sosa <isek7or@gmail.com>
2024-01-23 20:18:30 -0800

    completed all routes

--- routes/Animal.js
+++ routes/Animal.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const {
     getAllAnimals,
     getAnimalById,
-    createAnimal
+    createAnimal,
+    deleteAnimal,
+    updateAnimal
 } = require('../controllers/Animal')
 
 // get all Animals
@@ -13,5 +15,11 @@ router.get('/:id', getAnimalById)
 
 // create animal
 router.post('/', createAnimal)
+
+// delete animal
+router.delete('/:id', deleteAnimal)
+
+// update animal
+router.put('/:id', updateAnimal)
 
 module.exports = router
